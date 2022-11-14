e1c9e3a8b26936d6c270ff7ecc3676d60258cdd4
nima ghaferi <nima.ghaferi@gmail.com>
2022-11-14 20:46:20 +0330

    enable spring security

---
+++ spring-security-demo/src/main/java/com/nemo/spring/security/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.nemo.spring.security.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(users.username("nemo").password("123").roles("ADMIN"))
+                .withUser(users.username("mary").password("123").roles("EMPLOYEE"))
+                .withUser(users.username("susan").password("123").roles("MANAGER"));
+    }
+}
