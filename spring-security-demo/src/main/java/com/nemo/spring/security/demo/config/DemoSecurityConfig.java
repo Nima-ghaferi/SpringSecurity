002ac84665d39f175c5a0cdb63c4ed62db98af87
nima ghaferi <nima.ghaferi@gmail.com>
2022-11-18 09:49:21 +0330

    jdbc  auth

--- spring-security-demo/src/main/java/com/nemo/spring/security/demo/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/nemo/spring/security/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.nemo.spring.security.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,17 +8,26 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(users.username("nemo").password("123").roles("ADMIN", "MANAGER", "EMPLOYEE"))
-                .withUser(users.username("mary").password("123").roles("EMPLOYEE"))
-                .withUser(users.username("susan").password("123").roles("MANAGER", "EMPLOYEE"));
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+
+
+
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(users.username("nemo").password("123").roles("ADMIN", "MANAGER", "EMPLOYEE"))
+//                .withUser(users.username("mary").password("123").roles("EMPLOYEE"))
+//                .withUser(users.username("susan").password("123").roles("MANAGER", "EMPLOYEE"));
     }
 
     @Override
